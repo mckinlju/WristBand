faac5cc31eb04555b136461222741b925c7f005c
mckinlju <mckinlju@mail.uc.edu>
2023-09-25 18:55:40 -0400

    Final Program

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        int age = 0;
+        System.out.print("Please enter your age: ");
+        age = in.nextInt();
 
+        if(age >=21)
+        {
+            System.out.println("Congratulations, you get a wristband!");
+        }
     }
 }
